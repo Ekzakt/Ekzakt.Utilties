779b2979ccee94e257ffc3aeaf452d26b75eb5a3
EricJansen <EricJansen@LAPTOP-EJ>
2023-12-28 13:32:35 +0100

    Added some comments.

--- Ekzakt.Utilities/Helpers/VersionExtensions.cs
+++ Ekzakt.Utilities/Helpers/VersionExtensions.cs
@@ -2,6 +2,12 @@
 
 public static class VersionExtensions
 {
+    /// <summary>
+    /// Returns a formatted string with major, minor and build,
+    /// and onlyy a revision version when it is not zero.
+    /// </summary>
+    /// <param name="version"></param>
+    /// <returns>string</returns>
     public static string Format(this Version? version)
     {
         var revision = version?.Revision == 0 ? $".{version?.Revision}" : string.Empty;
