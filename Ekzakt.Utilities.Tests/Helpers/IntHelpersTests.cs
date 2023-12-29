a9ff32f3e68019b80b6ded5da54f771fcc367f55
EricJansen <EricJansen@LAPTOP-EJ>
2023-12-30 00:15:14 +0100

    Added IntHelper class and tests.

---
+++ Ekzakt.Utilities.Tests/Helpers/IntHelpersTests.cs
@@ -0,0 +1,60 @@
+﻿using Ekzakt.Utilities.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Formats.Asn1;
+
+namespace Ekzakt.Utilities.Helpers.Tests
+{
+    [TestClass()]
+    public class IntHelpersTests
+    {
+        [TestMethod()]
+        public void GetRandomInt_ShouldBeBetweenMinValueAndMaxValue()
+        {
+            int result = IntHelpers.GetRandomInt();
+
+            Assert.IsTrue(result >= int.MinValue && result <= int.MaxValue);
+        }
+
+
+        [TestMethod()]
+        public void GetRandomPositiveInt_ShouldBePositive()
+        {
+            int result = IntHelpers.GetRandomPositiveInt();
+
+            Assert.IsTrue(result >= 0 && result <= int.MaxValue);
+        }
+
+
+        [TestMethod()]
+        public void GetRandomNegativeInt_ShouldBeNegative()
+        {
+            int result = IntHelpers.GetRandomNegativeInt();
+
+            Assert.IsTrue(result < 0);
+        }
+
+
+        [TestMethod()]
+        public void GetRandomIntBetween_WithValidMinValueAndMaxValue()
+        {
+            int minValue = 10;
+            int maxValue = 20;
+
+            int result = IntHelpers.GetRandomIntBetween(minValue, maxValue);
+
+            Assert.IsTrue(result >= minValue && result <= maxValue);
+        }
+
+
+        [TestMethod()]
+        public void GetRandomIntBetween_WithMinValueAndMaxValueReversed()
+        {
+            int minValue = 200;
+            int maxValue = 10;
+
+            int result = IntHelpers.GetRandomIntBetween(minValue, maxValue);
+
+            Assert.IsTrue(result >= maxValue && result <= minValue);
+        }
+    }
+}
---
+++ Ekzakt.Utilities/Helpers/IntHelpers.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+
+namespace Ekzakt.Utilities.Helpers;
+
+public static class IntHelpers
+{
+    /// <summary>
+    /// Returns a random value between minValue and maxValue.
+    /// </summary>
+    /// <param name="minValue">The minimum value, included.</param>
+    /// <param name="maxValue">The maximum value, included.</param>
+    /// <returns>int</returns>
+    public static int GetRandomIntBetween(int minValue, int maxValue)
+    {
+        int min = minValue > maxValue ? maxValue : minValue;
+        int max = maxValue > minValue ? maxValue : minValue;
+
+        Random random = new Random();
+
+        var result = random.Next(min, max);
+
+        return result;
+    }
+
+
+    /// <summary>
+    /// Returns a random int value between int.MinValue and int.MaxValue.
+    /// </summary>
+    /// <returns>int</returns>
+    public static int GetRandomInt()
+    {
+        return GetRandomIntBetween(int.MinValue, int.MaxValue);
+    }
+
+
+    /// <summary>
+    /// Returns a random positive int value, zero not included.
+    /// </summary>
+    /// <returns>int</returns>
+    public static int GetRandomPositiveInt()
+    {
+        return GetRandomIntBetween(1, int.MaxValue);
+    }
+
+
+    /// <summary>
+    /// Returns a random negative int value, zero not included.
+    /// </summary>
+    /// <returns>int</returns>
+    public static int GetRandomNegativeInt()
+    {
+        return GetRandomIntBetween(int.MinValue, -1);
+    }
+}
