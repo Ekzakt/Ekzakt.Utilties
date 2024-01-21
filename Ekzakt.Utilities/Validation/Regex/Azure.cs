f306c9d2b29872cc62b798166e44807cbbdc6733
EricJansen <EricJansen@LAPTOP-EJ>
2024-01-21 19:59:26 +0100

    Added Validation.Regex namespace.

---
+++ Ekzakt.Utilities/Validation/Regex/Azure.cs
@@ -0,0 +1,23 @@
+﻿namespace Ekzakt.Utilities.Validation.Regex;
+
+public static class Azure
+{
+    public static class StorageAccount
+    {
+        /// <summary>
+        /// This regex matches Azure storage account names ensuring the 
+        /// length is between 3 and 24 characters with only lowercase letters and numbers.
+        /// The storage account name should be globally available.
+        /// </summary>
+        public const string ACCOUNT_NAME = "^[a-z0-9]{3,24}$";
+
+
+        /// <summary>
+        /// This regex matches Azure storage account container names ensuring the
+        /// langth is between 3 and 63 characters long, and starting and ending 
+        /// with a letter or number, containing only letters, numbers, and hyphens, 
+        /// with hyphens not allowed to be consecutive.
+        /// </summary>
+        public const string CONTAINERNAME = "^[a-z0-9](?:[a-z0-9]|-(?=[a-z0-9])){1,61}[a-z0-9]$";
+    }
+}
---
+++ Ekzakt.Utilities/Validation/Regex/Internet.cs
@@ -0,0 +1,19 @@
+﻿namespace Ekzakt.Utilities.Validation.Regex;
+
+public static class Internet
+{
+    /// <summary>
+    /// This regex matches domain names ensuring that they consist of 
+    /// valid alphanumeric characters, may contain hyphens, and have a 
+    /// valid structure with dots separating the domain labels.
+    /// </summary>
+    public const string HOST_NAME = @"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$";
+
+
+    /// <summary>
+    /// this regex matches IPv4 addressses ensuring that each octet is a 
+    /// valid number between 0 and 255 and that there are three such 
+    /// octets separated by dots. 
+    /// </summary>
+    public const string IPv4_ADDRESS = @"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+}
