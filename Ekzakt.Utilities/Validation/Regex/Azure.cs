ac31fcc8147d1674cb2d861eb9cc942025dd8a6c
EricJansen <EricJansen@LAPTOP-EJ>
2024-01-30 00:49:14 +0100

    Obsoleted regex constants.

--- Ekzakt.Utilities/Validation/Regex/Azure.cs
+++ Ekzakt.Utilities/Validation/Regex/Azure.cs
@@ -9,7 +9,26 @@ public static class Azure
         /// length is between 3 and 24 characters with only lowercase letters and numbers.
         /// The storage account name should be globally available.
         /// </summary>
-        public const string ACCOUNT_NAME = "^[a-z0-9]{3,24}$";
+        public const string STORAGE_ACCOUNT_NAME = "^[a-z0-9]{3,24}$";
+
+
+
+        /// <summary>
+        /// This regex matches Azure blob client names.
+        /// See <see href="https://learn.microsoft.com/en-us/rest/api/storageservices/naming-and-referencing-containers--blobs--and-metadata">the
+        /// Microsoft documentation</see> for more information.
+        /// </summary>
+        public const string BLOB_CLIENT_NAME = @"^(?!.*[/\\.]$)(?!.*[\\/.]$)(?!.*[\\/.]{2,})([^\x00-\x1F\x7F\""*+,:;<=>?@\[\\\]^`{|}]|\%[0-9a-fA-F]{2}){1,1024}$";
+
+
+
+        /// <summary>
+        /// This regex matches Azure storage account container names ensuring the
+        /// langth is between 3 and 63 characters long, and starting and ending 
+        /// with a letter or number, containing only letters, numbers, and hyphens, 
+        /// with hyphens not allowed to be consecutive.
+        /// </summary>
+        public const string BLOB_CONTAINER_NAME = "^[a-z0-9](?:[a-z0-9]|-(?=[a-z0-9])){1,61}[a-z0-9]$";
 
 
         /// <summary>
@@ -28,15 +47,17 @@ public static class Azure
         /// with a letter or number, containing only letters, numbers, and hyphens, 
         /// with hyphens not allowed to be consecutive.
         /// </summary>
+        [Obsolete("Use BLOB_CONTAINER_NAME instead.")]
         public const string CONTAINER_NAME = "^[a-z0-9](?:[a-z0-9]|-(?=[a-z0-9])){1,61}[a-z0-9]$";
 
 
         /// <summary>
-        /// This regex matches Azure blob client names.
-        /// See <see href="https://learn.microsoft.com/en-us/rest/api/storageservices/naming-and-referencing-containers--blobs--and-metadata">the
-        /// Microsoft documentation</see> for more information.
+        /// This regex matches Azure storage account names ensuring the 
+        /// length is between 3 and 24 characters with only lowercase letters and numbers.
+        /// The storage account name should be globally available.
         /// </summary>
-        public const string BLOB_CLIENT_NAME = @"^(?!.*[/\\.]$)(?!.*[\\/.]$)(?!.*[\\/.]{2,})([^\x00-\x1F\x7F\""*+,:;<=>?@\[\\\]^`{|}]|\%[0-9a-fA-F]{2}){1,1024}$";
+        [Obsolete("Use STORAGE_ACCOUNT_NAME instead.")]
+        public const string ACCOUNT_NAME = "^[a-z0-9]{3,24}$";
 
     }
 }
