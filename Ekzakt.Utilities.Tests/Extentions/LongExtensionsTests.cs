9d422ae064a62656db57e6f1bb29f81174399cf9
EricJansen <EricJansen@LAPTOP-EJ>
2024-02-07 23:41:54 +0100

    Added optional decimal numbers to LongExtension method FormatFileSize.

---
+++ Ekzakt.Utilities.Tests/Extentions/LongExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ekzakt.Utilities.Extentions.Tests
+{
+    [TestClass()]
+    public class LongExtensionsTests
+    {
+        [TestMethod()]
+        public void FormatFileSizeTest_ShouldReturnKB()
+        {
+            long value1 = 1024;
+            long value2 = 15645;
+
+            var result1 = value1.FormatFileSize(2);
+            var result2 = value2.FormatFileSize(2);
+
+            Assert.IsTrue(result1 == "1,00 KB" && result2 == "15,28 KB");
+        }
+
+
+        [TestMethod()]
+        public void FormatFileSizeTest_ShouldReturnMB()
+        {
+            long value1 = 1048576;
+            long value2 = 1085426;
+
+            var result1 = value1.FormatFileSize(2);
+            var result2 = value2.FormatFileSize(2);
+
+            Assert.IsTrue(result1 == "1,00 MB" && result2 == "1,04 MB");
+        }
+    }
+}
--- Ekzakt.Utilities/Extentions/LongExtensions.cs
+++ Ekzakt.Utilities/Extentions/LongExtensions.cs
@@ -6,9 +6,21 @@ public static class LongExtensions
     /// Formats the given file size (long) in bytes into a human-readable 
     /// string representation with the appropriate unit (Bytes, KB, MB, GB, TB, PB, EB, ZB, YB).
     /// </summary>
+    [Obsolete("Use overload FormatFileSize(int decimalPlaces = 0) instead.")]
     public static string FormatFileSize(this long value)
     {
-        var bytes = value;
+        return value.FormatFileSize(0);
+    }
+
+
+    /// <summary>
+    /// Formats the given file size (long) in bytes into a human-readable 
+    /// string representation with the appropriate unit (Bytes, KB, MB, GB, TB, PB, EB, ZB, YB).
+    /// <param name="decimalPlaces"/>
+    /// </summary>
+    public static string FormatFileSize(this long value, int decimalPlaces = 0)
+    {
+        var bytes = (decimal)value;
 
         string[] sizeSuffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
         const int scale = 1024;
@@ -20,6 +32,7 @@ public static class LongExtensions
             order++;
         }
 
-        return $"{bytes} {sizeSuffixes[order]}";
+        string formatString = $"{{0:N{decimalPlaces}}} {{1}}";
+        return string.Format(formatString, bytes, sizeSuffixes[order]);
     }
 }
