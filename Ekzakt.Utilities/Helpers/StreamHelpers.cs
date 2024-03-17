62b97ee2aab1a8940749cf472e89062a21ac1f14
EricJansen <EricJansen@LAPTOP-EJ>
2024-03-17 17:19:56 +0100

    Changed method name StreamHelpers.GenerateStreamFromString to CreateStream.

--- Ekzakt.Utilities/Helpers/StreamHelpers.cs
+++ Ekzakt.Utilities/Helpers/StreamHelpers.cs
@@ -8,7 +8,28 @@ public static class StreamHelpers
     /// </summary>
     /// <param name="value"></param>
     /// <returns>Stream</returns>
+    [Obsolete("Use CreateStream instead. This method will be removed in a future version.")]
     public static Stream GenerateStreamFromString(string value)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+
+        writer.Write(value);
+        writer.Flush();
+
+        stream.Position = 0;
+
+        return stream;
+    }
+
+
+    /// <summary>
+    /// Converts any string into a stream.  
+    /// Note: Don't forget to use using.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns>Stream</returns>
+    public static Stream CreateStream(string value)
     {
         var stream = new MemoryStream();
         var writer = new StreamWriter(stream);
